94d19d42b57b0fdbd6bf902c84d4e4f29125afee
hasanshrestha <hasan.xtha.5311@gmail.com>
2022-10-12 12:43:58 +0545

    settings updated

--- PdfSearch/settings.py
+++ PdfSearch/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o(s^0c6r2&zns3$4n6dedreg$zwfpx3($+2-=ung0g(())v^pg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["67.205.174.129"]
 
 
 # Application definition
@@ -75,14 +76,24 @@ WSGI_APPLICATION = "PdfSearch.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "pdf",
+#         "USER": "postgres",
+#         "PASSWORD": "pgadmin",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "pdf",
-        "USER": "postgres",
-        "PASSWORD": "pgadmin",
+        "NAME": "dhukutiDB",
+        "USER": "dhukuti_admin",
+        "PASSWORD": "admin@123",
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "",
     }
 }
 
@@ -117,13 +128,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-# to upload in folder
-MEDIA_ROOT = "PdfSearchApp/static/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# to upload in folder
+MEDIA_ROOT = "PdfSearchApp/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
